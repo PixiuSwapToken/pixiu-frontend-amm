ab60083c36ce374552205979309931d6f66ed697
pixiuswap <83927033+pixiuswap@users.noreply.github.com>
2021-06-21 22:10:48 +0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 // import { bsc, fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 import { injected, bsc } from '../connectors'
 // TODO
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
 
 // a list of tokens by chain
 type ChainTokenList = {
